f6940e3ea3b74c2f234e4dc66137a0185769c315
miftakhul_aziz <miftakhul.14116078@student.itera.ac.id>
2021-09-22 15:01:02 +0700

    v1

---
+++ face_detect_cv3.py
@@ -0,0 +1,20 @@
+import cv2, sys
+
+imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor = 1.1,
+    minNeighbors = 5,
+    minSize=(30,30)
+)
+print("Found {0} Faces".format(len(faces)))
+for(x,y,w,h) in faces:
+    cv2.rectangle(image,(x,y), (x+w, y+h), (0,255,0),2)
+cv2.imshow("faces found", image)
+cv2.waitKey(0)
+# NOW TEST IMAGE TEAM FCB
+# DONE
